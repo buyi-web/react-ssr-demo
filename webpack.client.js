633bdcccbaaecdaa18a9cdd05dee7969ad8d9825
wentao <wentao@sobey.com>
2021-09-24 15:17:39 +0800

    服务器渲染

---
+++ webpack.client.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const baseConfig = require("./webpack.base.");
+const { merge } = require("webpack-merge");
+const clientConfig = {
+    devtool: "source-map",
+    entry: "./src/client",
+    output: {
+        filename: "js/bundle.[hash:5].js",
+        path: path.resolve(__dirname, "./public")
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ]
+};
+
+module.exports = merge(baseConfig, clientConfig);
